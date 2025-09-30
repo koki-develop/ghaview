044988cda82a41dfedeabe078faf3b12432372bd
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 09:20:15 +0900

    feat: Implement starting server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Command } from "commander";
 import packageJson from "../package.json" with { type: "json" };
+import { startServer } from "./server.ts";
 
 const program = new Command();
 
 program
   .name("ghaview")
   .version(packageJson.version)
-  .action(() => {
-    console.log("hello world");
+  .action(async () => {
+    await startServer();
   });
 
 program.parse(process.argv);
