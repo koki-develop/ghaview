0081356f41fb506ee46c07f5b506d2335592b4ce
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 19:10:38 +0900

    chore: Add tailwindcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import devServer, { defaultOptions } from "@hono/vite-dev-server";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     devServer({
-      entry: "src/server.ts",
+      entry: "src/server/index.ts",
       exclude: [/^(?!\/api)/, ...defaultOptions.exclude],
     }),
   ],
