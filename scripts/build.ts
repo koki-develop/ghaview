724a6c96fb42bc4da262d8075b632c9f718c85d6
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 09:30:10 +0900

    fix: Fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,22 @@
 import { $ } from "bun";
 
+// Clean dist directory
 await $`rm -rf dist`;
 
+// Build CLI
 await Bun.build({
   banner: "#!/usr/bin/env node",
   entrypoints: ["./src/index.ts"],
   outdir: "./dist",
   target: "node",
   packages: "external",
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
 });
-
 await $`chmod +x ./dist/index.js`;
+
+// Build React app
+await $`bunx vite build`;
+await $`mkdir -p dist/web`;
+await $`cp vite.config.ts dist/web/`;
