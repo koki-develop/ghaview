bd3b98b454f7f278f4e1ae561c5f93ce19a32147
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 19:10:03 +0900

    chore: Add types

---
+++ src/lib/types.ts
@@ -0,0 +1,35 @@
+export type WorkflowFile = {
+  filename: string;
+  workflow: Workflow;
+};
+
+export type Workflow = {
+  name?: string;
+  needs?: string[];
+  jobs: Record<string, Job>;
+};
+
+export type Job = {
+  name?: string;
+  steps?: Step[];
+  permissions?: Permissions;
+};
+
+export type Permissions = Record<string, "read" | "write" | "none">;
+
+type StepBase = {
+  id?: string;
+  name?: string;
+  env?: Record<string, unknown>;
+};
+
+type RunStep = StepBase & {
+  run?: string;
+};
+
+type UsesStep = StepBase & {
+  uses?: string;
+  with?: Record<string, unknown>;
+};
+
+export type Step = RunStep | UsesStep;
