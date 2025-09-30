4071f109c8ab3148fc69fb964fe25347eb6e776e
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 21:05:34 +0900

    fix: Add source and target handles to JobNode component for better workflow visualization

--- src/web/pages/WorkflowPage/index.tsx
+++ src/web/pages/WorkflowPage/index.tsx
@@ -1,16 +1,28 @@
 import { Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Background, Panel, ReactFlow } from "@xyflow/react";
+import {
+  Background,
+  Handle,
+  MarkerType,
+  Panel,
+  Position,
+  ReactFlow,
+} from "@xyflow/react";
 import { useParams } from "react-router";
 import type { Job } from "../../../lib/types";
 import { useWorkflow } from "../../lib/workflows";
 import JobDrawer from "./JobDrawer";
 
-function JobNode({
-  data: { jobId, job },
-}: {
-  data: { jobId: string; job: Job };
+function JobNode(props: {
+  data: {
+    jobId: string;
+    job: Job;
+    hasSourceHandle: boolean;
+    hasTargetHandle: boolean;
+  };
 }) {
+  console.log({ props });
+  const { jobId, job, hasSourceHandle, hasTargetHandle } = props.data;
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
@@ -24,6 +36,17 @@ function JobNode({
       </button>
 
       <JobDrawer open={opened} onClose={close} jobId={jobId} job={job} />
+
+      {hasSourceHandle && (
+        <Handle
+          className="invisible"
+          type="source"
+          position={Position.Bottom}
+        />
+      )}
+      {hasTargetHandle && (
+        <Handle className="invisible" type="target" position={Position.Top} />
+      )}
     </div>
   );
 }
@@ -40,6 +63,20 @@ export default function WorkflowPage() {
   const { nodes, edges } = (() => {
     if (!workflow) return { nodes: [], edges: [] };
 
+    const edges = Object.entries(workflow.jobs).flatMap(([jobId, job]) => {
+      if (!job.needs) return [];
+      const needsArray = Array.isArray(job.needs) ? job.needs : [job.needs];
+
+      return needsArray.map((sourceJobId) => ({
+        id: `${sourceJobId}-${jobId}`,
+        source: sourceJobId,
+        target: jobId,
+        markerEnd: {
+          type: MarkerType.Arrow,
+        },
+      }));
+    });
+
     const nodes = Object.entries(workflow.jobs).map(([jobId, job], index) => ({
       id: jobId,
       type: "job",
@@ -47,10 +84,12 @@ export default function WorkflowPage() {
       data: {
         jobId,
         job,
+        hasSourceHandle: edges.some((edge) => edge.source === jobId),
+        hasTargetHandle: edges.some((edge) => edge.target === jobId),
       },
     }));
 
-    return { nodes, edges: [] };
+    return { nodes, edges };
   })();
 
   return (
