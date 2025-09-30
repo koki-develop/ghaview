1fc3e217753b8867419a1ffd65934ab363b319ff
koki-develop <kou.pg.0131@gmail.com>
2025-09-30 14:34:34 +0900

    chore: Add hono plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import devServer, { defaultOptions } from "@hono/vite-dev-server";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    devServer({
+      entry: "src/server.ts",
+      exclude: [/^(?!\/api)/, ...defaultOptions.exclude],
+    }),
+  ],
   build: {
     outDir: "dist/web",
   },
